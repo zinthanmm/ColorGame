2bcc056c145cc78a9f972c0b5a724fd6b3275061
zinthanmm <zinthanmm@gmail.com>
2020-04-15 16:42:12 +0530

    hard code - part 1

---
+++ colorGame.js
@@ -0,0 +1,16 @@
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)",
+];
+
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.querySelector("#colorDisplay");
+pickedColor = colors[3];
+
+// for(var i=0, i < squares.length, i++){
+//     squares[i].getElementsByClassName.backgroundColor = colors[i]
+// }
